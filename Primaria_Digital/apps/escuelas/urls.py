bd6a6029c7a915d9738a7c455c7ba9b6af572aad
Gustavo <gustavoezequieltroncoso@gmail.com>
2017-01-21 19:16:30 -0300

    Se creo nueva vista para listar las escuelas registradas en app Escuela

--- Primaria_Digital/apps/escuelas/urls.py
+++ Primaria_Digital/apps/escuelas/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import EscuelaCreateView,EscuelaUpdateView
+from .views import EscuelaCreateView,EscuelaUpdateView,ListaEscuelasView
 
 urlpatterns = [
     url(r'^alta/$', EscuelaCreateView.as_view(), name='alta'),
     url(r'^modificar/(?P<pk>[0-9]+)/$', EscuelaUpdateView.as_view(), name='modificar'),
+    url(r'^lista/$', ListaEscuelasView.as_view(), name='lista'),
 ]
--- Primaria_Digital/apps/escuelas/views.py
+++ Primaria_Digital/apps/escuelas/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse, reverse_lazy
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView,TemplateView
 from .models import Escuela
 from .forms import EscuelaForm
 
@@ -16,3 +16,11 @@ class EscuelaUpdateView(UpdateView):
     template_name = "alta.html"
     form_class = EscuelaForm
     success_url = reverse_lazy('main:home')
+
+class ListaEscuelasView(TemplateView):
+    template_name = "listado_colegios.html"
+
+    def get_context_data(self,**kwargs):
+        context = super(ListaEscuelasView,self).get_context_data(**kwargs)
+        context['escuelas'] = Escuela.objects.all()
+        return context
