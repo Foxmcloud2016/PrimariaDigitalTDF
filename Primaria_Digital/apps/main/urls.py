0f24f0967c3f5ba680855193f98d808f4703e38f
Gustavo <gustavoezequieltroncoso@gmail.com>
2017-01-21 19:25:35 -0300

    Agregado de LoginRequiredMixin a las paginas que no puede tener acceso el usuario, ya que no se encuentra logeado

--- Primaria_Digital/apps/main/urls.py
+++ Primaria_Digital/apps/main/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-from django.contrib.auth.decorators import login_required
+
 from .views import HomeView
 
 urlpatterns = [
-    url(r'^home/$', login_required(HomeView.as_view()), name='home'),
+    url(r'^home/', HomeView.as_view(), name='home'),
 ]
--- Primaria_Digital/apps/main/views.py
+++ Primaria_Digital/apps/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View, TemplateView
+from django.core.urlresolvers import reverse_lazy
 
-class HomeView(TemplateView):
+class HomeView(LoginRequiredMixin,TemplateView):
     template_name = "home.html"
+    login_url = reverse_lazy('users:login')
     
