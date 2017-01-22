cf61601cf35bd4a2781ff92ff644a3900e7df893
Gustavo <gustavoezequieltroncoso@gmail.com>
2017-01-21 23:41:05 -0300

    Se mejoro visualmente la aplicacion Web

--- Primaria_Digital/apps/escuelas/views.py
+++ Primaria_Digital/apps/escuelas/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.views.generic import CreateView, UpdateView,TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Escuela
 from .forms import EscuelaForm
 
 # Create your views here.
-class EscuelaCreateView(CreateView):
+class EscuelaCreateView(LoginRequiredMixin,CreateView):
     model = Escuela
     template_name = "alta.html"
     form_class = EscuelaForm
     success_url = reverse_lazy('main:home')
+    login_url = reverse_lazy('users:login')    
 
-class EscuelaUpdateView(UpdateView):
+class EscuelaUpdateView(LoginRequiredMixin,UpdateView):
     model = Escuela
     template_name = "alta.html"
     form_class = EscuelaForm
     success_url = reverse_lazy('main:home')
+    login_url = reverse_lazy('users:login')
 
-class ListaEscuelasView(TemplateView):
+class ListaEscuelasView(LoginRequiredMixin,TemplateView):
     template_name = "listado_colegios.html"
+    login_url = reverse_lazy('users:login')
 
     def get_context_data(self,**kwargs):
         context = super(ListaEscuelasView,self).get_context_data(**kwargs)
