ee428ea09dbe9bb8a8d570d327e361acd8e44321
Gustavo <gustavoezequieltroncoso@gmail.com>
2017-01-24 19:38:33 -0300

    Listado de acciones por usuario

--- Primaria_Digital/apps/acciones/urls.py
+++ Primaria_Digital/apps/acciones/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import AccionCreateView
+from .views import AccionCreateView, AccionListView
 
 urlpatterns = [
     url(r'^alta/', AccionCreateView.as_view(), name='alta'),
+    url(r'^lista/', AccionListView.as_view(), name='lista'),
 ]
--- Primaria_Digital/apps/acciones/views.py
+++ Primaria_Digital/apps/acciones/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.contrib.auth.models import User
@@ -14,13 +14,18 @@ class AccionCreateView(LoginRequiredMixin,CreateView):
     template_name = "alta_accion.html"
     login_url = reverse_lazy('users:login')
     form_class = AccionForm
-    success_url = reverse_lazy('acciones:alta')
+    success_url = reverse_lazy('acciones:lista')
 
     def form_valid(self,form):
         form.instance.usuario = self.request.user
         return super(AccionCreateView,self).form_valid(form)
 
+class AccionListView(ListView):
+    model = Accion
+    template_name = "lista_acciones.html"
 
+    def get_context_data(self,**kwargs):
+        context = super(AccionListView,self).get_context_data(**kwargs)
+        context['acciones'] = Accion.objects.filter(usuario = self.request.user)
+        return context
 
-
-    
