bd56f9569871d1f75c1e613e5d5213a24b080b8d
Gustavo <gustavoezequieltroncoso@gmail.com>
2017-02-23 22:12:51 -0300

    Deploying to Heroku

--- Primaria_Digital/Primaria_Digital/settings/prod.py
+++ Primaria_Digital/Primaria_Digital/settings/prod.py
@@ -2,8 +2,12 @@ from .base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
